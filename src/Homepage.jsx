cae851ead99e9f6ddc16ee785b08cc60debe633a
Nikita Parkhomenko <nikita.parkhom@gmail.com>
2020-09-12 14:35:09 +0300

    add homepage component

---
+++ src/Homepage.jsx
@@ -0,0 +1,40 @@
+import React from 'react';
+
+import './Homepage.scss';
+
+export const Homepage = () => (
+  <div className="homepage">
+    <div className="directory-menu">
+      <div className="menu-item">
+        <div className="content">
+          <h1 className="title">HATS</h1>
+          <span className="subtitle">BUY NOW</span>
+        </div>
+      </div>
+      <div className="menu-item">
+        <div className="content">
+          <h1 className="title">HATS</h1>
+          <span className="subtitle">BUY NOW</span>
+        </div>
+      </div>
+      <div className="menu-item">
+        <div className="content">
+          <h1 className="title">HATS</h1>
+          <span className="subtitle">BUY NOW</span>
+        </div>
+      </div>
+      <div className="menu-item">
+        <div className="content">
+          <h1 className="title">HATS</h1>
+          <span className="subtitle">BUY NOW</span>
+        </div>
+      </div>
+      <div className="menu-item">
+        <div className="content">
+          <h1 className="title">HATS</h1>
+          <span className="subtitle">BUY NOW</span>
+        </div>
+      </div>
+    </div>
+  </div>
+);
